c550f21c86be2c277c1bf804862b3ce2a50387ce
tuan <1278991950@qq.com>
2021-01-30 01:14:33 +0800

    取消原来在这配置的px2rem

--- build/utils.js
+++ build/utils.js
@@ -22,12 +22,12 @@ exports.cssLoaders = function (options) {
     }
   }
 
-  const px2remLoader = {
+  /*const px2remLoader = {
     loader: 'px2rem-loader',
     options: {
       remUnit: 75
     }
-  }
+  }*/
 
   const postcssLoader = {
     loader: 'postcss-loader',
@@ -38,8 +38,8 @@ exports.cssLoaders = function (options) {
 
   // generate loader string to be used with extract text plugin
   function generateLoaders (loader, loaderOptions) {
-    //const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
-    const loaders = [cssLoader,px2remLoader];
+    const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
+    //const loaders = [cssLoader,px2remLoader];
 
     if (loader) {
       loaders.push({
