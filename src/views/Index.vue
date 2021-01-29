bff3424a5fe8944865d39cc182c60a6358bc725d
tuan <1278991950@qq.com>
2021-01-29 12:00:51 +0800

    footer的颜色切换完善

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -9,17 +9,17 @@
         <el-row type="flex" class="row-bg">
           <el-col :span="24">
             <div @click="footer_clickIndex">
-              <i ref="footer_myIndex" class="icon iconfont" style="font-size:30px;text-align:center;color:skyblue">&#xe601;</i>
+              <i class="icon iconfont" style="font-size:30px;text-align:center;" :style="{color:indexColor}">&#xe601;</i>
             </div>
           </el-col>
           <el-col :span="24">
             <div @click="footer_clickApp">
-              <i ref="footer_myApp" class="icon iconfont" style="font-size:30px;text-align:center;">&#xe602;</i>
+              <i class="icon iconfont" style="font-size:30px;text-align:center;" :style="{color:appColor}">&#xe602;</i>
             </div>
           </el-col>
           <el-col :span="24">
             <div  @click="footer_clickUser">
-              <i ref="footer_myUser" class="icon iconfont" style="font-size:30px;text-align:center;">&#xe63a;</i>
+              <i class="icon iconfont" style="font-size:30px;text-align:center;" :style="{color:userColor}">&#xe63a;</i>
             </div>
           </el-col>
         </el-row>
@@ -32,42 +32,55 @@
 </template>
 
 <script>
-
+  import GLOBAL from '../api/global_variable'
     export default{
         name:'Index',
         data(){
             return{
+
+                indexColor: '',
+                appColor: '',
+                userColor: '',
                 footer_click_color:{
                     clicked: 'skyblue',
                     unClicked: ''
                 },
             }
         },
+        created(){
+          var currentIndex = this.$route.params.currentIndex;
+          if(currentIndex === undefined || currentIndex === 0){
+              this.indexColor = this.footer_click_color.clicked;
+          }else if(currentIndex === 1){
+              this.appColor = this.footer_click_color.clicked;
+          }else if(currentIndex === 2){
+              this.userColor = this.footer_click_color.clicked;
+          }
+        },
         methods:{
             footer_clickIndex(){
                 var vm = this;
                 //console.log(123);
                 //切换颜色
-                this.$refs.footer_myApp.style.color = this.footer_click_color.unClicked;
-                this.$refs.footer_myUser.style.color = this.footer_click_color.unClicked;
-                this.$refs.footer_myIndex.style.color = this.footer_click_color.clicked;
+                this.indexColor = this.footer_click_color.clicked;
+                this.appColor = this.footer_click_color.unClicked;
+                this.userColor = this.footer_click_color.unClicked;
                 this.$router.push('/main/myIndex');
             },
             footer_clickApp(){
                 var vm = this;
                 //切换颜色
-                this.$refs.footer_myIndex.style.color = this.footer_click_color.unClicked;
-                this.$refs.footer_myUser.style.color = this.footer_click_color.unClicked;
-                this.$refs.footer_myApp.style.color = this.footer_click_color.clicked;
+                this.indexColor = this.footer_click_color.unClicked;
+                this.appColor = this.footer_click_color.clicked;
+                this.userColor = this.footer_click_color.unClicked;
                 this.$router.push('/main/myApp');
-
             },
             footer_clickUser(){
                 var vm = this;
                 //切换颜色
-                this.$refs.footer_myIndex.style.color = this.footer_click_color.unClicked;
-                this.$refs.footer_myApp.style.color = this.footer_click_color.unClicked;
-                this.$refs.footer_myUser.style.color = this.footer_click_color.clicked;
+                this.indexColor = this.footer_click_color.unClicked;
+                this.appColor = this.footer_click_color.unClicked;
+                this.userColor = this.footer_click_color.clicked;
                 this.$router.push('/main/myUser');
             }
         }
@@ -85,8 +98,6 @@
   .el-main {
     background-color: #E9EEF3;
     color: #333;
-    text-align: center;
-    line-height: 1200px;
     padding: 0 0 0 0;
   }
 
