9301aca7d92965b1773c49f2badd09109fe42af5
tuan <1278991950@qq.com>
2021-02-24 11:11:35 +0800

    用户详情功能，已完成，其中包括用户头像替换

--- src/components/user/MyDetail.vue
+++ src/components/user/MyDetail.vue
@@ -15,7 +15,21 @@
             :show-file-list="false">
               <img src="http://localhost:8087/userImg/defaultImg.jpg" class="avatar">
             </el-upload>-->
-            <van-uploader
+            <el-upload
+              class="avatar-uploader"
+              :data="data"
+              :action="updateImgUrl"
+              accept="image/*"
+              :headers="headers"
+              :limit="1"
+              :show-file-list="false"
+              :on-error="handleAvatarError"
+              :on-success="handleAvatarSuccess"
+              :before-upload="beforeAvatarUpload">
+                <img v-if="userImg" :src="userImg" class="avatar">
+                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+            <!--<van-uploader
               preview-size="60px"
               :max-count="1"
               accept="image/*"
@@ -27,8 +41,10 @@
                 width="60px"
                 height="60px"
                 :src="userImg"
-              />
-            </van-uploader>
+              >
+                <template v-slot:error>加载失败</template>
+              </van-image>
+            </van-uploader>-->
           </span>
         </div>
         <div>
@@ -42,6 +58,12 @@
           <span>账&#9号</span>
           <span class="floatRight">
             {{userId}}
+          </span>
+        </div>
+        <div>
+          <span>邮&#9箱</span>
+          <span class="floatRight">
+            {{email}}
           </span>
         </div>
         <div>
@@ -67,24 +89,70 @@
 
 <script>
   import GLOBAL from '../../api/global_variable';
+  import {Toast} from 'vant';
     export default {
         name: "MyDetail",
         data(){
             return{
-                nickName:'哈哈哈哈哈',
-                userId:123,
+                data:{
+                    'userId': window.sessionStorage.getItem('userId')
+                },
+                headers:{
+                    'Authorization': window.sessionStorage.getItem('token')
+                },
+                updateImgUrl: GLOBAL.httpBaseUrl + "/user/updateImage",
+                nickName:'',
+                userId:'',
+                email: '',
                 schoolName:'-1',
                 schoolNameColor:'#808A87',
                 toSelectSchool:true,
                 num:'-1',
                 numColor:'#808A87',
                 toFinishNum: true,
-                userImg: GLOBAL.httpBaseUrl + "/userImg/defaultImg.jpg"
+                userImg: ''
             }
         },
         created(){
+            let userId = window.sessionStorage.getItem('userId');
+            this.userId = userId;
+            let vm = this;
             //发送数据请求
-            if(this.schoolName === '-1'){
+            this.axios({
+                url: '/user/getUserDetailInfo',
+                method: 'get',
+                params:{
+                    userId: userId
+                }
+            }).then(function(res){
+               if(res.data.code === 200) {
+                   vm.userImg = GLOBAL.httpBaseUrl + res.data.data.image;
+                   vm.nickName = res.data.data.nickName;
+                   vm.schoolName = res.data.data.schoolName;
+                   vm.email = res.data.data.email;
+                   vm.num = res.data.data.schoolNum;
+
+                   if(vm.schoolName === '-1'){
+                       vm.schoolName = "未绑定";
+                       vm.schoolNameColor = '#808A87';
+                   }else{
+                       vm.schoolNameColor = '#FFAAFF';
+                       vm.toSelectSchool = false;
+                   }
+                   if(vm.num === '-1'){
+                       vm.num = "未绑定";
+                       vm.numColor = '#808A87';
+                   }else{
+                       vm.numColor = '#FFAAFF';
+                       vm.toFinishNum = false;
+                   }
+
+               }else{
+                   Toast.fail(res.data.msg);
+               }
+            });
+
+            /*if(this.schoolName === '-1'){
                 this.schoolName = "未绑定";
                 this.schoolNameColor = '#808A87';
             }else{
@@ -97,7 +165,7 @@
             }else{
                 this.numColor = '#FFAAFF';
                 this.toFinishNum = false;
-            }
+            }*/
         },
         methods:{
             toBack(){
@@ -126,9 +194,57 @@
             finishNum(){
                 this.$router.push("/user/finishNum");
             },
-            afterRead(file){
-                console.log(file);
-            }
+            /*afterRead(file){
+                //console.log(file);
+                let vm = this;
+                var formData = new FormData();
+                formData.append("file",file);
+                this.axios({
+                    url: '/user/updateImage',
+                    method: 'post',
+                    data: formData,
+                    processData: false, //默认为true。默认情况下，发送的数据将被转换为对象,这里要发送的是binary类型
+                    //cache: false,
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+
+                    }
+                }).then(function (res) {
+                    if(res.data.code === 200){
+                        Toast.success("更换成功");
+                    }else{
+                        Toast.fail(res.data.msg);
+                    }
+                }).catch(function (error) {
+                    Toast.fail("服务器异常");
+                })
+            },*/
+            handleAvatarSuccess(response,file,fileList){
+                console.log(response);
+                //console.log(file);
+                //console.log(fileList);
+                if(response.code === 200){
+                    this.userImg = GLOBAL.httpBaseUrl + response.data+"?timeout";
+                    Toast.success("更新成功");
+                }else{
+                    Toast.fail(response.msg);
+                }
+            },
+            handleAvatarError(response,file,fileList){
+                Toast.fail("图片上传失败");
+            },
+            beforeAvatarUpload(file){
+                //console.log(file);
+                if(file.type.indexOf("image") === -1){
+                    Toast.fail("请选择图片");
+                    return false;
+                }else if(file.size > 5242880){
+                    Toast.fail("图片不能超过5兆");
+                    return false
+                }
+                return true;
+
+            },
         }
     }
 </script>
@@ -191,8 +307,8 @@
     float: right;
   }
   .avatar {
-    width: 90px;
-    height: 90px;
+    width: 120px;
+    height: 120px;
     float: right;
   }
   .img span>div{
