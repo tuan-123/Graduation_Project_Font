471ec2b2a206813730c2358489644de71b0e4c98
tuan <1278991950@qq.com>
2021-03-13 22:15:24 +0800

    空白页，根据外部地址显示内容

---
+++ src/components/blankPage/Link.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+      <div style="position: absolute;top:50%;left:10px;z-index: 10">
+        <van-icon size="25" name="arrow-left" @click="back"/>
+      </div>
+      <iframe :src="url" id="MyFrame" scrolling="auto" frameborder="0"></iframe>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Link",
+        data(){
+            return{
+                url: '',
+                fileList: [],
+            }
+        },
+        created(){
+            this.url = this.$route.params.url;
+        },
+        methods:{
+            back(){
+                this.$router.push("/index");
+            },
+        }
+    }
+</script>
+
+<style scoped>
+  .container{
+    width: 100%;
+    height: 100%;
+  }
+  .container #MyFrame{
+    width: 100%;
+    height: 100%;
+  }
+</style>
