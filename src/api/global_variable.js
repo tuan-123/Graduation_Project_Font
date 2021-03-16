8c02b9d6699bce491f3e8a16549079c249377c0c
tuan <1278991950@qq.com>
2021-03-16 19:47:39 +0800

    添加webSocket

--- src/api/global_variable.js
+++ src/api/global_variable.js
@@ -2,7 +2,18 @@ const httpBaseUrl = 'http://127.0.0.1:8087';
 //const httpBaseUrl = 'http://192.168.2.104:8087';
 const footerCurrentClicked = 0;
 
+const webSocket = null;
+const webSocketTimeOutObj = null;
+const webSocketServerTimeOutObj = null;
+const webSocketTimeOut = 15000;
+const lockReconnect = false;
+
 export default {
   httpBaseUrl,
-  footerCurrentClicked
+  footerCurrentClicked,
+  webSocket,
+  webSocketTimeOutObj,
+  webSocketServerTimeOutObj,
+  webSocketTimeOut,
+  lockReconnect,
 }
