01405d0888344ca5edceacdeb80aca398c6e1e6e
tuan <1278991950@qq.com>
2021-03-16 19:46:34 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -18,20 +18,39 @@ import 'lib-flexible'
 //导入自定义全局函数模块
 import GlobalMethods from './api/global_methods'
 
-/*//使用Vant-ui 按需导入
-import Toast from "vant/lib/toast"
-import 'vant/lib/style/base.css'
-import 'vant/lib/toast/style'
-import Dialog from "vant/lib/dialog"
-import 'vant/lib/dialog/style'
-import Notify from "vant/lib/notify"
-import 'vant/lib/notify/style'*/
 //导入vant所有组件
 import Vant from 'vant'
 import 'vant/lib/index.css'
+import {Lazyload} from 'vant'
 
+//导入NutUi
+import NutUI from '@nutui/nutui'
+import '@nutui/nutui/dist/nutui.css'
 
+NutUI.install(Vue);
 
+import MyUi from './components'
+Vue.use(MyUi);
+
+//不手动引入地图不显示
+import 'echarts/map/js/china'
+import 'echarts/map/js/world'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios,axios);
+Vue.config.productionTip = false;
+//配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8087';
+//axios.defaults.baseURL = 'http://192.168.2.104:8087';
+
+//通过axios的拦截器添加token验证
+axios.interceptors.request.use(config =>{
+  //console.log(config);
+  config.headers.Authorization = window.sessionStorage.getItem('token');
+  return config;
+});
 
 
 
@@ -39,8 +58,10 @@ Vue.use(VueRouter);
 Vue.use(ElementUI);
 Vue.use(GlobalMethods);
 Vue.use(Vant);
+Vue.use(Lazyload);
 
 Vue.config.productionTip = false;
+
 
 /* eslint-disable no-new */
 new Vue({
